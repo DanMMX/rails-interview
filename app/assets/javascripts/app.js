5cb98335e49b50b47aa98f45eb498e23f688f3df
Dan Rocha <drochag1@gmail.com>
2017-03-21 01:01:27 -0600

    Using bootstrap

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,17 @@
 angular.module('interviewApp', [])
 .controller('MainCtrl', [function () {
   this.test = 'Hello world!';
+  this.students = [{
+    name: 'Dan',
+    movie: 'bla'
+  }, {
+    name: 'Dan',
+    movie: 'bla'
+  }, {
+    name: 'Dan',
+    movie: 'bla'
+  }, {
+    name: 'Dan',
+    movie: 'bla'
+  }];
 }]);
