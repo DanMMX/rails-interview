e2303c8cc1a8139aa72cea88b4011ebc7268ab7d
Dan Rocha <drochag1@gmail.com>
2017-03-20 18:57:34 -0600

    Name and Movie accesor. Repeated name with lastname initial. Filtered students by favorite movie

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,15 @@
 class Student < ActiveRecord::Base
+  scope :favoritemovie, -> (param) { where('favoritemovie like ?', param) }
+
+  def name
+    has_repeated_name? ? "#{firstname} #{lastname[0]}." : firstname
+  end
+
+  def movie
+    favoritemovie
+  end
+
+  def has_repeated_name?
+    Student.where(:firstname => self.firstname).count > 1
+  end
 end
